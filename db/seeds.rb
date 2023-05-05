e644bddf2453a4e0aa772ba04ff15b2fea9b1997
Lucas Gruner <lucas.gruner@gmail.com>
2023-05-05 11:41:35 +0200

    seed basic tasks with description and completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Destroying all records"
+Task.destroy_all
+
+puts "Records destroyed. Database contains #{Task.all.size} tasks."
+
+verbs = %w[Clean Tidy Organise Sort Trash Buy Purchase Plan Lend Facilitate Call Email Convince Apply Finish]
+objects = %w[car forst house garage tools kitchen inbox computer phone apartment building entrance apps games]
+places = %w[France Alsace Toronto Canada Berlin school university office ]
+
+100.times do
+  description = "#{verbs.sample} #{objects.sample} in #{places.sample}"
+  task = Task.create(description: description, completed: [true, false].sample)
+  puts "Created task '#{task.description}' (id: #{task.id})"
+end
+
+puts "Database contains #{Task.all.size} tasks."
